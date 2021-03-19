245761650d2871b9e186096ad276b812502f2c50
Francisco Javier Acevedo Guerrero <francisco.acevedo@perceptio.net>
2021-03-18 22:44:21 -0500

    Actualización sin TestBed

--- src/app/image-gallery/image-gallery.component.spec.ts
+++ src/app/image-gallery/image-gallery.component.spec.ts
@@ -1,9 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ImageService } from '../image.service';
 
 import { GalleryComponent } from './image-gallery.component';
 
 xdescribe('ImageGalleryComponent', () => {
-  let component: GalleryComponent;
+/*   let component: GalleryComponent;
   let fixture: ComponentFixture<GalleryComponent>;
 
   beforeEach(async(() => {
@@ -18,8 +19,23 @@ xdescribe('ImageGalleryComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+ */
+  let component: GalleryComponent;
+  let service : ImageService;
+  //const service = new ImageService();
+  beforeEach(() => {
+    service = new ImageService();
+    component = new GalleryComponent(service);
+  });
 
-  it('should create', () => {
+  it('Componente creado', () => {
     expect(component).toBeTruthy();
   });
+
+   it('Componente creado', () => {
+     //Sin terminar
+    component.ngOnChanges();
+    expect(component.filterBy).toBeTruthy();
+  });
+
 });
