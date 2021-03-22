09f0ea7451c026a1efbaba7bdcbe1bda3df450a8
Francisco Javier Acevedo Guerrero <francisco.acevedo@perceptio.net>
2021-03-21 21:14:39 -0500

    Actualización image-gallery component

--- src/app/image-gallery/image-gallery.component.spec.ts
+++ src/app/image-gallery/image-gallery.component.spec.ts
@@ -3,7 +3,7 @@ import { ImageService } from '../image.service';
 
 import { GalleryComponent } from './image-gallery.component';
 
-xdescribe('ImageGalleryComponent', () => {
+describe('ImageGalleryComponent', () => {
 /*   let component: GalleryComponent;
   let fixture: ComponentFixture<GalleryComponent>;
 
@@ -22,23 +22,40 @@ xdescribe('ImageGalleryComponent', () => {
  */
   let component: GalleryComponent;
   let service : ImageService;
+  let fixture: ComponentFixture<GalleryComponent>;
   //const service = new ImageService();
   beforeEach(() => {
-    service = new ImageService();
-    component = new GalleryComponent(service);
+    //service = new ImageService();
+    //component = new GalleryComponent(service);
+
+     // refine the test module by declaring the test component
+     TestBed.configureTestingModule({
+      declarations: [GalleryComponent],
+      providers: [ImageService]
+    });
+
+    fixture = TestBed.createComponent(GalleryComponent);
+    component = fixture.componentInstance;
+    service = TestBed.get(ImageService);
   });
 
   it('Componente creado', () => {
+    service = new ImageService();
+    component = new GalleryComponent(service);
     expect(component).toBeTruthy();
   });
 
    it('Todas las imagenes son perro', () => {
      //Sin terminar
-     component.filterBy = 'perro';
-     component.ngOnChanges();
+     
+     
+     //component.ngOnChanges();
      //FOR allimages
      // expect(imagen.brand).toBe("perro"); 
      //ENDFOR
+     component.filterBy = 'perro';
+     fixture.detectChanges();
+     component.ngOnChanges();
      for (let imagen of component.allImages){
       expect(imagen.brand).toBe("perro"); 
      }
