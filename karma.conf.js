0e7afac967408ab71b098718ad0247ce0310d06f
grillo8585 <grillo8585@gmail.com>
2021-05-09 23:27:33 -0500

    Update karma.conf.js
    Otro cambio

--- karma.conf.js
+++ karma.conf.js
@@ -81,7 +81,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Firefox','Chrome'],
+    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
