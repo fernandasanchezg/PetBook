155da8e71791bff09b4986532e6cb07b32ab48a4
grillo8585 <grillo8585@gmail.com>
2021-05-09 23:41:46 -0500

    Update karma.conf.js
    Original

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,8 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -32,3 +37,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
