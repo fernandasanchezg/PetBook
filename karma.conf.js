dc685ca5007a9d019bb7594acc8cbfaac57bee3c
grillo8585 <grillo8585@gmail.com>
2021-05-09 23:35:18 -0500

    Update karma.conf.js
    Perfecto

--- karma.conf.js
+++ karma.conf.js
@@ -1,66 +1,16 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
-process.env.CHROME_BIN = require('puppeteer').executablePath()
-
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['detectBrowsers','jasmine', '@angular-devkit/build-angular'], //Adicion inicial
-    // configuration
-    detectBrowsers: {
-      // enable/disable, default is true
-      enabled: true,
-
-      // enable/disable phantomjs support, default is true
-      usePhantomJS: true,
-
-      // use headless mode, for browsers that support it, default is false
-      preferHeadless: true,
-
-      // post processing of browsers list
-      // here you can edit the list of browsers used by karma
-      postDetection: function(availableBrowsers) {
-        /* Karma configuration with custom launchers
-          customLaunchers: {
-            IE9: {
-              base: 'IE',
-              'x-ua-compatible': 'IE=EmulateIE9'
-            }
-          }
-        */
-
-          //Add IE Emulation
-          var result = availableBrowsers;
-
-          if (availableBrowsers.indexOf('IE')>-1) {
-            result.push('IE9');
-          }
-
-          //Remove PhantomJS if another browser has been detected
-          if (availableBrowsers.length > 1 && availableBrowsers.indexOf('PhantomJS')>-1) {
-            var i = result.indexOf('PhantomJS');
-
-            if (i !== -1) {
-              result.splice(i, 1);
-            }
-          }
-
-          return result;
-        }
-    },
     customLaunchers:{
       HeadlessChrome:{
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
     },
-    //frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [ //Adicion inicial
-      require('karma-firefox-launcher'),
-      require('karma-detect-browsers'), //Adicion Final
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
       require('karma-jasmine'),
-      //require('karma-chrome-launcher'),
+      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
@@ -82,7 +32,3 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
-  });
-};
